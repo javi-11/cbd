6b1a8330acaeff3e3d7aa89da3937ad575641d05
javi-11 <javiterrobaorozco@gmail.com>
2023-05-06 13:20:06 +0200

    Version previa de los detalles

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 import json
 from flask import Blueprint, jsonify, redirect, render_template, request, url_for
 from pymongo import MongoClient
-from bson import json_util
+from bson import ObjectId, json_util
 client = MongoClient('mongodb://localhost:27017/')
 mongo = client.ProyectoCBD
 
@@ -14,9 +14,29 @@ def home():
     response = json.loads(response)
     return render_template("index.html", name = "Javi", recetas = response)
 
+@views.route("/<userid>")
+def detalles(userid):
+    user=userid[10:34:1]
+    
+    receta = mongo.A.find({'_id': ObjectId(user)})
+    response=json_util.dumps(receta)
+    
+    response = json.loads(response)
+    Aux = response[0]
+    tags=""
+    for  tag in Aux.get("tags"):
+        if tag == Aux.get("tags")[-1]:     
+            tags=tags + tag +""
+        else:
+            tags=tags + tag +", "
+
+
+
+    return render_template("details.html", receta = response, tags = tags)
 
 @views.route("/filter/<username>")
 def home2(username):
+
     return render_template("index.html", name = username)
 
 @views.route("/data")
