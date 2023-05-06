92a9f2bdfa67506bfabbf26b7e794fe2bbadf11d
javi-11 <javiterrobaorozco@gmail.com>
2023-05-06 11:41:27 +0200

    first commit

---
+++ views.py
@@ -0,0 +1,30 @@
+import json
+from flask import Blueprint, jsonify, redirect, render_template, request, url_for
+from pymongo import MongoClient
+from bson import json_util
+client = MongoClient('mongodb://localhost:27017/')
+mongo = client.ProyectoCBD
+
+views = Blueprint(__name__, "views")
+
+@views.route("/")
+def home():
+    recetas = mongo.A.find()
+    response = json_util.dumps(recetas)
+    response = json.loads(response)
+    return render_template("index.html", name = "Javi", recetas = response)
+
+
+@views.route("/filter/<username>")
+def home2(username):
+    return render_template("index.html", name = username)
+
+@views.route("/data")
+def getData():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go_to_home")
+def go_to_home():
+
+    return redirect(url_for("views.home"))
