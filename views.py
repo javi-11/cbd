087ae32b14ef21fbcce37aabd717a716e2c6f9ab
javi-11 <javiterrobaorozco@gmail.com>
2023-05-07 14:26:39 +0200

    Eliminación de error de campo de prueba

--- views.py
+++ views.py
@@ -19,7 +19,7 @@ def home():
     }}])
     response = json_util.dumps(recetas)
     response = json.loads(response)
-    return render_template("index.html", name = "Javi", recetas = response)
+    return render_template("index.html", recetas = response)
 
 
 @views.route("/tags")
@@ -32,14 +32,14 @@ def recetasXEtiquetas():
     response = json_util.dumps(recetas)
     response = json.loads(response)
     print(response)
-    return render_template("topTags.html", name = "Javi", recetas = response)
+    return render_template("topTags.html", recetas = response)
 
 @views.route("/sort")
 def sort():
     recetas = mongo.A.aggregate([{"$sort": {"receta":1}}])
     response = json_util.dumps(recetas)
     response = json.loads(response)
-    return render_template("sort.html", name = "Javi", recetas = response)
+    return render_template("sort.html",  recetas = response)
 
 @views.route("/mostComment")
 def topComment():
@@ -50,7 +50,7 @@ def topComment():
     {"$limit": 3}])
     response = json_util.dumps(recetas)
     response = json.loads(response)
-    return render_template("TopComments.html", name = "Javi", recetas = response)
+    return render_template("TopComments.html", recetas = response)
 
 
 @views.route("/mostRated")
@@ -58,7 +58,7 @@ def topPuntuaciones():
     recetas = mongo.A.aggregate([{"$project": {"me_gusta": 1,"receta":1,"autor_info.name":1,"ingredientes":1,"tags":1,"numElements": { "$size": "$me_gusta" }}},{ "$sort": { "numElements": -1 } },{ "$limit": 3 }])
     response = json_util.dumps(recetas)
     response = json.loads(response)
-    return render_template("TopPuntuaciones.html", name = "Javi", recetas = response)
+    return render_template("TopPuntuaciones.html", ecetas = response)
 
 @views.route("/search")
 def filter():
@@ -91,7 +91,7 @@ def filter():
         response = json.loads(response)
         response2 = json.loads(response2)
         rsp = response2 + response
-        return render_template("index.html", name = "Javi", recetas = rsp)
+        return render_template("index.html", recetas = rsp)
 
     else:
 
@@ -103,7 +103,7 @@ def filter():
         }}])
         response = json_util.dumps(recetas)
         response = json.loads(response)
-        return render_template("index.html", name = "Javi", recetas = response)
+        return render_template("index.html", recetas = response)
     
     
 
